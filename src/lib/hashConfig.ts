043032ef966af3e5a79c9dc0910958381950ad7a
Ash Kyd <kyd.ashley@abc.net.au>
2025-04-15 08:16:54 +1000

    bug: don't add version to hash when not in builder

--- src/lib/hashConfig.ts
+++ src/lib/hashConfig.ts
@@ -194,9 +194,11 @@ export const hashConfig = makeSvelteStore<{
 }>(schema);
 
 // Version our hash. This way we can introduce/deprecate/upgrade features as needed.
-hashConfig.subscribe(val => {
-  const CURRENT_VERSION = 1;
-  if (val && val.version !== CURRENT_VERSION) {
-    hashConfig.set({ ...val, version: CURRENT_VERSION });
-  }
-});
+if (window.location.pathname.includes('/builder/')) {
+  hashConfig.subscribe(val => {
+    const CURRENT_VERSION = 1;
+    if (val && val.version !== CURRENT_VERSION) {
+      hashConfig.set({ ...val, version: CURRENT_VERSION });
+    }
+  });
+}
